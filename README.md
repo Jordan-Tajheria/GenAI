ce535ff5e1ae5fd4ea7c4078de8f58beb5739186
Jordan-Tajheria <jordan.roberts@publicissapient.com>
2023-10-10 14:37:39 +0100

    Rudimentary chatbot set up to read dataset

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. Install OpenAPI / Check for updates as not all computers may have the plougins for OpenAPI.
 
 2. Install streamlit
+
+3. Install langchain
+
+4. Install dotenv
+
+5. Install tabulate
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+
 def main():
+
+    load_dotenv()
+
     #Let streamlit know that this is a streamlit application
     st.set_page_config(page_title="Generative AI Chatbot")
     st.header("Generative AI Chatbot")
@@ -8,6 +16,20 @@ def main():
     #Store in an object
     user_csv = st.file_uploader("Upload your CSV file", type="csv")
 
+    #Check if the user has provided a dataset, in this case a csv file
+    if user_csv is not None:
+        user_question = st.text_input("Ask any question about your CSV")
+
+        #Initialise the LLM, 0=non-creative, 1=creative (I.e returning a different answer each time)
+        llm = OpenAI(temperature=0)
+        #Verbose set to true will display the thinking/thought process the model is undertaking whilst
+        # generating the answer.
+        agent = create_csv_agent(llm, user_csv, verbose=True)
+
+        if user_question is not None and user_question != "":
+            response = agent.run(user_question)
+            #st.write(f"Your question was: {user_question}")
+            st.write(response)
 
 #Test whether we are importing the application
 if __name__ == "__main__":
